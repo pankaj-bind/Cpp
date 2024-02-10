ba0c05b23962b93856de1ef7f4d99b365c621b48
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-10 14:36:16 +0530

    define added

---
+++ variables/define.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+#define PI 3.14
+
+int main(){
+    int radius = 43;
+    double area = 2 * PI * radius;
+    cout<<"The area of the circle is " << area;
+    return 0;
+}
