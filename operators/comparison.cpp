039b9fa3c614d8207cda7b9c64b1cee581459eb3
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-12 22:49:13 +0530

    comparison operator added

---
+++ operators/comparison.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int x=10;
+    int y=20;
+
+    cout<< (x==y)<<"\n";
+    cout<< (x!=y)<<"\n";
+    cout<< (x>y)<<"\n";
+    cout<< (x<y)<<"\n";
+    cout<< (x>=y)<<"\n";
+    cout<< (x<=y)<<"\n";
+    return 0;
+}
