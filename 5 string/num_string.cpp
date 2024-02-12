23dda3861a1f6c6727e99ec343817c445c9ef5bf
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-12 23:02:44 +0530

    num_string added

---
+++ 5 string/num_string.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str1 = "Pankaj";
+    string str2 = "Kumar";
+    string str3 = "Bind";
+
+    cout<<str1+" "+str2+" "+str3;
+    // cout<<(str1+1);  num+string=error
+
+    return 0;
+}
