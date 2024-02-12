bc6005f750ffd10573ef5ade28f39253dc37349d
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-12 22:45:12 +0530

    assignment operator added:

---
+++ operators/assignment.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=20;
+
+    x+=1;
+    cout<<x<<"\n";
+
+    x-=1;
+    cout<<x<<"\n";
+
+    x*=2;
+    cout<<x<<"\n";
+
+    x/=3;
+    cout<<x<<"\n";
+
+    x%=2;
+    cout<<x<<"\n";
+
+    x&=2;
+    cout<<x<<"\n";
+
+    x|=2;
+    cout<<x<<"\n";
+
+    x!=2;
+    cout<<x<<"\n";
+
+    x^=2;
+    cout<<x<<"\n";
+
+    x>>=2;
+    cout<<x<<"\n";
+
+    x<<=2;
+    cout<<x<<"\n";
+    return 0;
+}
