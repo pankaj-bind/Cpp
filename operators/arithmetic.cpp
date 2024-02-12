010135b30c0c39e828f4c03d3ef6e526e4168f84
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-12 22:38:30 +0530

    arithmetic operators added

---
+++ operators/arithmetic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=10;
+    int y=20;
+
+    cout<<x+y<<"\n";
+    cout<<x-y<<"\n";
+    cout<<x*y<<"\n";
+    cout<<x/y<<"\n";
+    cout<<x%y<<"\n";
+    cout<<x++<<"\n";
+    cout<<y--<<"\n";
+
+    return 0;
+}
