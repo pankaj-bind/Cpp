37b33858f1dbf319464c931876663ea78c75ebaa
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-12 23:14:21 +0530

    string access added

---
+++ 5 string/stringaccess.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout<<str[0]<<"\n";
+    cout<<str[1]<<"\n";
+    cout<<str[2]<<"\n";
+    cout<<str[3]<<"\n";
+    cout<<str[4]<<"\n";
+    cout<<str[5]<<"\n";
+    cout<<str[6]<<"\n";
+    cout<<str[7]<<"\n";
+    cout<<str[8]<<"\n";
+    cout<<str[9]<<"\n";
+    cout<<str[10]<<"\n";
+
+    return 0;
+}
