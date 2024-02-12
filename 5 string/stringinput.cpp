93892afdfe64b6a3b5e9f1fcb2184f5b6905ddbf
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-12 23:30:56 +0530

    string input added

---
+++ 5 string/stringinput.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string firstName;
+    string lastName;
+    cout<<"Enter your first name : ";cin>>firstName;
+    cout<<"Enter your last name : ";cin>>lastName;
+
+    cout<<"Your full name is "<<firstName<<" "<<lastName;
+    return 0;
+}
