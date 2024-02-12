8ca16dd7deeb3605bebfaa06bcf3c8bb8c82adc9
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-12 23:06:45 +0530

    string length added

---
+++ 5 string/stringlength.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    string str1 = "Pankaj";
+    string str2 = "Kumar";
+    string str3 = "Bind";
+    string str4 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    cout<<str1.length()<<"\n";
+    cout<<str2.length()<<"\n";
+    cout<<str3.length()<<"\n";
+    cout<<str4.length()<<"\n";
+    return 0;
+}
