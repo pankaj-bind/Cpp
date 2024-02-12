6d5529e8e86931e7eb1ce687d71f8e2dd31f49b5
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-12 23:53:36 +0530

    if else added

---
+++ 7 conditions/ifelse.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter your age: ";cin>>age;
+
+    if(age<18){
+        cout<<"You are not eligible for voting";
+    } else{
+        cout<<"You are eligible for voting";
+    }
+
+    return 0;
+}
