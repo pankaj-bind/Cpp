38d077066e38aabe7758618f63a6d482ae3bd510
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-14 22:05:27 +0530

    multiple parameter function added

---
+++ 14 function/multipleparameter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void name(string name, int age){
+    cout<<name<<" "<<age<<"\n";
+}
+int main(){
+    name("Pankaj", 20);
+    name("Sharavan", 21);
+    name("Ram Lakhan", 20);
+    name("Jeetu", 20);
+    return 0;
+}
