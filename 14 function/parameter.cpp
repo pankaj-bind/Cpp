fd2de00f1a3a0ca224e736237e2ca39fc6ab1d5b
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-14 22:00:21 +0530

    function with parameter

---
+++ 14 function/parameter.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void name(string name){
+    cout<<name<<"\n";
+}
+int main(){
+    name("Pankaj");
+    name("Sharavan");
+    name("Ram Lakhan");
+    name("Jeetu");
+    return 0;
+}
