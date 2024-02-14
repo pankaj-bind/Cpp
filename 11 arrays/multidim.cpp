38ce08c95a5de704f9d43a6105d47c7d4bce5438
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-14 20:15:21 +0530

    multi dimensional array added

---
+++ 11 arrays/multidim.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr1[5][5] = {{1,2,3,4,5}, {6,7,8,9,0}};
+
+    for(int i=0; i<2; i++){
+        for(int j=0; j<5; j++){
+            cout<<arr1[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+
+    int arr2[5][5] = {{1,2,3,4,5}, {6,7,8,9,0}};
+
+}
