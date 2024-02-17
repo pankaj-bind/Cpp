a26c6c004dd154204ee0b62586d1733cbbe22d8b
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-17 20:11:58 +0530

    problem 4 added

--- 15 Practice Problem/problem2.cpp
+++ 15 Practice Problem/problem2.cpp
@@ -2,8 +2,6 @@
 using namespace std;
 
 int main(){
-    int num1 = 29;
-    int num2 = 30;
-    cout<<"The sum of "<<num1<<" and "<<num2<<" is: "<<num1+num2;
+    cout<<"The sum of "<<29<<" and "<<30<<" is: "<<29+30;
     return 0;
 }
