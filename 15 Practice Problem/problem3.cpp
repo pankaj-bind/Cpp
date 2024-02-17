4c97a81937874e7732ab54c8786acd49918b12db
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-17 20:09:02 +0530

    problem3 added

---
+++ 15 Practice Problem/problem3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Find Size of fundamental data types :\n";
+    cout<<"-------------------------------------\n";
+    cout<<"The sizeof(char) is : "<<sizeof(byte)<<" bytes\n";
+    cout<<"The sizeof(short) is : "<<sizeof(short)<<" bytes\n";
+    cout<<"The sizeof(int) is : "<<sizeof(int)<<" bytes\n";
+    cout<<"The sizeof(long) is : "<<sizeof(long)<<" bytes\n";
+    cout<<"The sizeof(long long) is : "<<sizeof(long long)<<" bytes\n";
+    cout<<"The sizeof(float) is : "<<sizeof(float)<<" bytes\n";
+    cout<<"The sizeof(double) is : "<<sizeof(double)<<" bytes\n";
+    cout<<"The sizeof(bool) is : "<<sizeof(bool)<<" bytes\n";
+    return 0;
+}
