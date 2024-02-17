caba67894284b064428a054a3b22930ab8bfd253
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-17 20:40:22 +0530

    problem 7 added

---
+++ 15 Practice Problem/problem7.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Display arithmetic operations with mixed data type :"<<endl;
+    cout<<"---------------------------------------------------"<<endl;
+
+    cout<<5<<"+"<<7<<" = "<<5+7<<endl;
+    cout<<3.7<<"+"<<8.0<<" = "<<3.7+8.0<<endl;
+    cout<<5<<"+"<<8.0<<" = "<<5+8.0<<endl;
+    cout<<5<<"-"<<7<<" = "<<5-7<<endl;
+    cout<<3.7<<"-"<<8.0<<" = "<<3.7-8.0<<endl;
+    cout<<5<<"-"<<8.0<<" = "<<5-8.0<<endl;
+    cout<<5<<"*"<<7<<" = "<<5*7<<endl;
+    cout<<3.7<<"*"<<8.0<<" = "<<3.7*8.0<<endl;
+    cout<<5<<"*"<<8.0<<" = "<<5*8.0<<endl;
+    cout<<5<<"/"<<7<<" = "<<5/7<<endl;
+    cout<<3.7<<"/"<<8.0<<" = "<<3.7/8.0<<endl;
+    cout<<5<<"/"<<8.0<<" = "<<5/8.0<<endl;
+    
+    return 0;
+}
