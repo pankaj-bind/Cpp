437097c0d499df9a84c3fe2e7f9a15c20d001074
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-17 22:34:21 +0530

    problem 12 added

---
+++ 15 Practice Problem/problem12.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Sum of two numbers : "<<endl;
+    cout<<"---------------------"<<endl;
+    int num1, num2;
+    cout<<"Input 1st number : "; cin>>num1;
+    cout<<"Input 2nd number : "; cin>>num2;
+    cout<<"The sum of the numbers is : "<<num1+num2;
+    return 0;
+}
