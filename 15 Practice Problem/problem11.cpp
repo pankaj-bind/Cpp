0a5d96b329698c5ff0683073f5719098cae4848b
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-17 22:30:05 +0530

    problem 11 added

---
+++ 15 Practice Problem/problem11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Sample Output:"<<endl;
+    cout<<"Print the result of some specific operation :"<<endl;
+
+    cout<<"Result of 1st expression is :"<<(2*3/4*7/9-9+8)<<endl;
+    cout<<"Result of 2nd expression is :"<<((35+5)%7)<<endl;
+    cout<<"Result of 3rd expression is :"<<(14+-4*6/11)<<endl;
+    cout<<"Result of 4th expression is :"<<(2+15/6*1-7%2)<<endl;
+    return 0;
+}
