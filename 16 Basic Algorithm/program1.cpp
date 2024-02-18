c815391e727693a9a5cdec1c9bbe36cce046bb77
Pankaj Kumar Bind <pankajbind30@gmail.com>
2024-02-18 20:25:24 +0530

    program 1 added

---
+++ 16 Basic Algorithm/program1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void output(int n1, int n2){
+    cout<<(n1+n2);
+}
+int main(){
+    int num1, num2;
+    cin>>num1;
+    cin>>num2;
+    output(num1, num2);
+    return 0;
+}
